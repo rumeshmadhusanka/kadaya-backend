7f643431cd7fcc7cd0ed91b04c959258d3710976
rumesh <rumesh.m.s16@gmail.com>
2020-04-21 23:31:49 +0530

    create new order changed

--- models/order_model.js
+++ models/order_model.js
@@ -4,102 +4,108 @@ const Buyer = require('./buyer_model');
 let buyer_obj1 = new Buyer();
 
 class Order {
-    getOrderHistoryByIdForBuyer(buyer_id) {
-        return buyer_obj1.getBuyingHistory(buyer_id)
-    }
+	getOrderHistoryByIdForBuyer(buyer_id) {
+		return buyer_obj1.getBuyingHistory(buyer_id)
+	}
 
-    getOngoingOrdersByIdForBuyer(buyer_id) {
-        return buyer_obj1.getOngoingOrders(buyer_id)
-    }
+	getOngoingOrdersByIdForBuyer(buyer_id) {
+		return buyer_obj1.getOngoingOrders(buyer_id)
+	}
 
-    async getOngoingOrdersByIdForShop(shop_id) {
-        let query_str = 'SELECT * FROM public.order where shop_id=($1) and state!=($2)';
+	async getOngoingOrdersByIdForShop(shop_id) {
+		let query_str = 'SELECT * FROM public.order where shop_id=($1) and state!=($2)';
 
-        return new Promise(async (resolve, reject) => {
-            try {
-                let {rows} = await pg_pool.query(query_str, [shop_id,"COMPLETED"]);
-                //console.log(rows);
-                resolve(rows);
-            } catch (e) {
-                reject(e)
-            }
-        })
-    }
+		return new Promise(async (resolve, reject) => {
+			try {
+				let {rows} = await pg_pool.query(query_str, [shop_id, "COMPLETED"]);
+				//console.log(rows);
+				resolve(rows);
+			} catch (e) {
+				reject(e)
+			}
+		})
+	}
 
-   async  getOrderHistoryByIdForShop(shop_id) {
-       let query_str = 'SELECT * FROM public.order where shop_id=($1) and state=($2)';
-       return new Promise(async (resolve, reject) => {
-           try {
-               let {rows} = await pg_pool.query(query_str, [shop_id,"COMPLETED"]);
-               //console.log(rows);
-               resolve(rows);
-           } catch (e) {
-               reject(e)
-           }
-       })
-    }
+	async getOrderHistoryByIdForShop(shop_id) {
+		let query_str = 'SELECT * FROM public.order where shop_id=($1) and state=($2)';
+		return new Promise(async (resolve, reject) => {
+			try {
+				let {rows} = await pg_pool.query(query_str, [shop_id, "COMPLETED"]);
+				//console.log(rows);
+				resolve(rows);
+			} catch (e) {
+				reject(e)
+			}
+		})
+	}
 
 
+	async createNewOrder(id, buyer_id, shop_id, message_body, started, last_updated_timestamp, total_price, items_list) {
+		let query_str = 'INSERT INTO public.order (id,buyer_id, shop_id, message_body, started, last_updated_timestamp, total_price) values (($1),($2),($3),($4),($5),($6))';
+		let query2 = 'INSERT INTO public.order_item (order_id, item_id, amount, price) values (($1),($2),($3),(select price from kadaya.public.item where id=kadaya.public.order_item.item_id))';
+		let query3 = 'UPDATE  public.order set total_price = (select sum(price) from kadaya.public.order_item where order_id = ($1)) where id=($1)';
+		return new Promise(async (resolve, reject) => {
+			try {
+				let result = await pg_pool.query(query_str, [id, buyer_id, shop_id, message_body, started, last_updated_timestamp, total_price]);
+				console.log(result);
+				if (result.rowCount === 1) {
+					let prom_list = [];
+					for (let i = 0; i < items_list.length; i++) {
+						prom_list.push(pg_pool.query(query2, [id, items_list[i].item_id, items_list[i].amount]));
+					}
+					Promise.all(prom_list).then(async (rs) => {
+						let bool_val = true;
+						for (let i = 0; i < rs.length; i++) {
+							bool_val = bool_val && (rs[i].rowCount === 1);
+						}
+						if (bool_val) {
+							let last = await pg_pool.query(query3, [id]);
+							if (last.rowCount === 1) {
+								resolve(true)
+							} else {
+								reject(new Error("Total value calculation error"))
+							}
 
-    async createNewOrder(id, buyer_id, shop_id, message_body, started, last_updated_timestamp, total_price, items_list) {
-        let query_str = 'INSERT INTO public.order (id,buyer_id, shop_id, message_body, started, last_updated_timestamp, total_price) values (($1),($2),($3),($4),($5),($6))';
-        let query2 = 'INSERT INTO public.order_item (order_id, item_id, amount, price) values (($1),($2),($3),($4))';
-        return new Promise(async (resolve, reject) => {
-            try {
-                let result = await pg_pool.query(query_str, [id, buyer_id, shop_id, message_body, started, last_updated_timestamp, total_price]);
-                console.log(result);
-                if (result.rowCount === 1) {
-                    let prom_list = [];
-                    for (let i = 0; i < items_list.length; i++) {
-                        prom_list.push(pg_pool.query(query2, [items_list[i].order_id, items_list[i].item_id, items_list[i].amount, items_list[i].price]));
-                    }
-                    Promise.all(prom_list).then((rs) => {
-                        let bool_val = true;
-                        for (let i = 0; i < rs.length; i++) {
-                            bool_val = bool_val && (rs[i].rowCount===1);
-                        }
-                        if (bool_val) {
-                            resolve(true)
-                        } else {
-                            reject(new Error("Some items could not be added"));
-                        }
-                    })
-                } else {
-                    reject(new Error("Could not create a new order"));
-                }
-            } catch (e) {
-                reject(e)
-            }
+						} else {
+							reject(new Error("Some items could not be added"));
+						}
+					})
+				} else {
+					reject(new Error("Could not create a new order"));
+				}
+			} catch (e) {
+				reject(e)
+			}
 
-        })
-    }
+		})
+	}
 
-    async updateOrderStatus(state, id) {
-        let query_str = "UPDATE public.order set state =($1), last_updated_timestamp=($2) where id=($3)";
-        let last_updated_timestamp = new Date().getTime();
-        return new Promise(async (resolve, reject) => {
-            try {
-                let result = await pg_pool.query(query_str, [state, last_updated_timestamp, id]);
-                console.log(result);
-                resolve(result);
-            } catch (e) {
-                reject(e)
-            }
-        })
-    }
+	async updateOrderStatus(state, id) {
+		let query_str = "UPDATE public.order set state =($1), last_updated_timestamp=($2) where id=($3)";
+		let last_updated_timestamp = new Date().getTime();
+		return new Promise(async (resolve, reject) => {
+			try {
+				let result = await pg_pool.query(query_str, [state, last_updated_timestamp, id]);
+				console.log(result);
+				resolve(result);
+			} catch (e) {
+				reject(e)
+			}
+		})
+	}
 
-    async deleteOrder(id) {
-        let query_str = "DELETE FROM public.order where id=($1)";
-        return new Promise(async (resolve, reject) => {
-            try {
-                let result = await pg_pool.query(query_str, [id]);
-                console.log(result);
-                resolve(result);
-            } catch (e) {
-                reject(e)
-            }
-        })
-    }
+	async deleteOrder(id) {
+		let query_str = "DELETE FROM public.order where id=($1)";
+		return new Promise(async (resolve, reject) => {
+			try {
+				let result = await pg_pool.query(query_str, [id]);
+				console.log(result);
+				resolve(result);
+			} catch (e) {
+				reject(e)
+			}
+		})
+	}
 
 }
 
