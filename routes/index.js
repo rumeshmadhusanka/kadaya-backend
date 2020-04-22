7f4b37f3b4975848f061b498000455888b3f1509
rumesh <rumesh.m.s16@gmail.com>
2020-04-22 18:42:07 +0530

    added order route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
     app.use("/shop", require("./shop"));
     app.use("/category", require("./category"));
     app.use("/monitoring",require("./monitoring"));
+    app.use("/order",require("./order"));
     app.use("/", (req, res) => {
         res.status(404).json({"message": "Default Route"})
     });
