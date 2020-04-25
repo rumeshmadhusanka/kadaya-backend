3a93041cc26657eb46f7b750f14324959f9e5e58
rumesh <rumesh.m.s16@gmail.com>
2020-04-25 17:52:02 +0530

    images return url on post

--- routes/image.js
+++ routes/image.js
@@ -10,8 +10,8 @@ const Image = require('../models/file_model');
 let image_obj = new Image();
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 let s3 = new AWS.S3();
 
@@ -43,59 +43,61 @@ let s3 = new AWS.S3();
 // });
 
 
-router.get('/shop/:shop_id/keys',async(req, res)=>{
-    let shop_id = req.params['shop_id'];
-    try{
-        let result =  await image_obj.getAllImageKeysOfAShop(shop_id);
-        await res.json(result)
-    }catch (e) {
-        await res.status(502).json({"msg": e.name+" "+e.message})
-    }
+router.get('/shop/:shop_id/keys', async (req, res) => {
+	let shop_id = req.params['shop_id'];
+	try {
+		let result = await image_obj.getAllImageKeysOfAShop(shop_id);
+		await res.json(result)
+	} catch (e) {
+		await res.status(502).json({"msg": e.name + " " + e.message})
+	}
 });
 
 let upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_S3_BUCKET,
-        acl: 'public-read',
-        metadata: function (req, file, cb) {
-            console.log(file);
-            cb(null, {fieldName: file.fieldname});
-        },
-        key: function (req, file, cb) {
-            let file_name = file.originalname;
-            file_name = file_name.split('.');
-            let extension = file_name[file_name.length-1];
-            cb(null,  "shop/"+uniqueId()+"."+extension)
-        }
-    })
+	storage: multerS3({
+		s3: s3,
+		bucket: process.env.AWS_S3_BUCKET,
+		acl: 'public-read',
+		metadata: function (req, file, cb) {
+			console.log(file);
+			cb(null, {fieldName: file.fieldname});
+		},
+		key: function (req, file, cb) {
+			let file_name = file.originalname;
+			file_name = file_name.split('.');
+			let extension = file_name[file_name.length - 1];
+			cb(null, "shop/" + uniqueId() + "." + extension)
+		}
+	})
 });
 
-router.post('/shop/:shop_id', upload.array('image', 5), async (req, res, next)=> {
-    let shop_id = req.params.shop_id;
-    try{
-        console.log("Inside ");
-        console.log(req.files);
-        for (let i = 0; i < req.files.length; i++) {
-            // save the file name to database
-             await image_obj.saveUploadUrl(req.files[i].location,shop_id)
-        }
-        await res.json({"msg": req.files.length})
-    }catch (e) {
-        await res.status(502).json({"msg": e.name + " " + e.message})
-    }
+router.post('/shop/:shop_id', upload.array('image', 5), async (req, res, next) => {
+	let shop_id = req.params.shop_id;
+	try {
+		console.log("Inside ");
+		console.log(req.files);
+		let keys_list = [];
+		for (let i = 0; i < req.files.length; i++) {
+			// save the file name to database
+			await image_obj.saveUploadUrl(req.files[i].location, shop_id);
+			keys_list.push(req.files[i].location)
+		}
+		await res.json({"msg": req.files.length, "keys": keys_list})
+	} catch (e) {
+		await res.status(502).json({"msg": e.name + " " + e.message})
+	}
 
 });
 
-router.delete('/shop/:shop_id/:url',  async (req, res, next)=> {
-    let url = req.params.url;
-    let shop_id = req.params.shop_id;
-    try{
-        console.log("Inside ");
-        await res.json({"msg":await image_obj.deleteImage(url,shop_id)})
-    }catch (e) {
-        await res.status(502).json({"msg": e.name + " " + e.message})
-    }
+router.delete('/shop/:shop_id/:url', async (req, res, next) => {
+	let url = req.params.url;
+	let shop_id = req.params.shop_id;
+	try {
+		console.log("Inside ");
+		await res.json({"msg": await image_obj.deleteImage(url, shop_id)})
+	} catch (e) {
+		await res.status(502).json({"msg": e.name + " " + e.message})
+	}
 
 });
 
